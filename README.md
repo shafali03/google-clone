3e0babbf6e20e32872c40e12e828671aaa15a00c
shafali03 <shaf03sm@gmail.com>
2020-09-20 20:18:44 +0100

    Created header structure with links

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Google clone
+
+yarn packages
+
+```
+yarn add @material-ui/core
+yarn add @material-ui/icons
+yarn add react-router-dom
+```
---
+++ src/components/Pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className='home'>
+      <h2>home</h2>
+      <div className="home__header">
+        <div className="home__headerLeft">
+          <Link to='/about'>About</Link>
+          <Link to='/store'>Store</Link>
+        </div>
+        <div className="home__headerRight">
+          <Link to='/gmail'>Gmail</Link>
+          <Link to='/images'>Images</Link>
+        </div>
+      </div>
+
+      <div className="home__body">
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
